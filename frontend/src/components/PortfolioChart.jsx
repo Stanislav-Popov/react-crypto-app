349b835c88c433db86fa76cef2847e2043edb45d
Stanislav Popov <it.is.the.pochta@gmail.com>
2025-04-21 17:10:22 +0400

    version 1

--- frontend/src/components/PortfolioChart.jsx
+++ frontend/src/components/PortfolioChart.jsx
@@ -15,8 +15,8 @@ export default function PortfolioChart() {
                 label: "$",
                 data: assets.map((a) => a.totalAmount.toFixed(2)),
                 backgroundColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
+                    "rgb(97, 73, 78)",
+                    "rgb(57, 69, 78)",
                     "rgba(255, 206, 86, 1)",
                     "rgba(75, 192, 192, 1)",
                     "rgba(153, 102, 255, 1)",
